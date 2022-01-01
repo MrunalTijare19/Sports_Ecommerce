e960e47fe8a2df015ac0dcb376f11bdb387bf851
Mrunal <lanurm123@gmail.com>
2022-01-01 16:51:23 +0530

    Add index

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w7*knmwhlnah4wz3_nguv(d*0+a#j$ertnrb73iw-xsom0zwa-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
